0147c69c4e0e300d9746f7c72355697fa27247d4
estedosunidos <ivanestebanvargasmartinez@gmail.com>
2025-02-07 02:39:57 +0100

    Add project files.

---
+++ CapaEntidy/DISTRITO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidy
+{
+   public  class DISTRITO
+    {
+        public int IdDistrito { get; set; }
+        public string Descripcion{get; set;}
+}
+}
---
+++ CapaEntidy/DetalleVenta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidy
+{
+    public class DetalleVenta
+    {
+  
+        public int IdDetalleVenta { get; set; }
+
+        public int IdVenta { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Total { get; set; }
+        public  Producto oProducto { get; set; }
+        public string IdTransaccion { get; set; }
+    }
+}
